497025a28dafae707505d632cdf56cd3bbf75414
wjf <13158643032qq.com>
2017-02-28 11:03:35 +0800

    branch23

--- app/src/main/java/com/example/qifan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/qifan/myapplication/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    
+    public void branch2(){
+        System.out.println(" = ");
+    }
 }
